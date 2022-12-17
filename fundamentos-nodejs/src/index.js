cb46b6c5adb6f88451566b2b6e586dae162c10f4
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-12-17 18:39:26 -0300

    feat: Finishing

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -1,6 +1,7 @@
 const express  = require('express')
 
 const app = express()
+app.use(express.json())
 
 app.get('/',(request,response)=>{
     return response.json({message: "Hello World Ignite! - FUndamentos NodeJS!!"})
